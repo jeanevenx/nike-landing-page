a35e8f89c119af26b51d7a9f3c6e2da0d7426ce5
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-08-28 18:21:29 -0300

    Module to export the sections

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import PopularProducts from './PopularProducts';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+
+export {
+    Hero,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    PopularProducts,
+    CustomerReviews,
+    Footer
+}
