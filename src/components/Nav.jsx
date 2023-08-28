15842eff6bd34198d27c0b31c744c2a1abc66afc
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-08-28 18:21:03 -0300

    Implementing the sections

---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import {headerLogo} from '../assets/images';
+
+const Nav = () => {
+  return (
+    <header>
+        <Nav>
+            <a href="/">
+                <img 
+                    src={headerLogo}
+                />
+
+            </a>
+        </Nav>
+    </header>
+  )
+}
+
+export default Nav
---
+++ src/sections/PopularProducts.jsx
@@ -0,0 +1,7 @@
+const popularProducts = () => {
+  return (
+    <div>popularProducts</div>
+  )
+}
+
+export default popularProducts
