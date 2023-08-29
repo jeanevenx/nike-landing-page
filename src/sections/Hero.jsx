59f34fc7aeda6b12e6442c490e4529b60eb434fe
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-08-29 19:12:46 -0300

    Implementing the 'Hero' section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,13 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col 
+        border-2 border-red-500 p-2 justify-center min-h-screen
+        gap-10"
+    >
+
+    </section>
   )
 }
 
