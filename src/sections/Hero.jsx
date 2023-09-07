d1205488409d6fdb419e8f62a4cbe5b3846a15fc
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-09-07 17:51:50 -0300

    shoe collection

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,15 @@
 import Button from '../components/Button';
+import ShoeCard from '../components/ShoeCard';
+
+import { useState } from 'react';
+
 import { arrowRight } from '../assets/icons';
-import {statistics } from '../constants';
+import {shoes, statistics } from '../constants';
+import { bigShoe1 } from '../assets/images';
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
+
   return (
     <section
       id="home"
@@ -10,21 +17,28 @@ const Hero = () => {
         justify-center min-h-screen
         gap-10 max-container"
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center 
-        items-start w-full max-xl:padding-x pt-28"
+      <div className="relative xl:w-2/5 flex flex-col 
+        justify-center items-start w-full 
+        max-xl:padding-x pt-28"
       >
-        <p className="text-xl font-montserrat text-coral-red">
+        <p className="text-xl font-montserrat  
+          text-coral-red">
           Our Summer collection
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px]
-          max-sm:leading-[82] font-bold"
+        <h1 className="mt-10 font-palanquin text-8xl 
+          max-sm:text-[72px] max-sm:leading-[82px] font-bold"
         >
-          <span>The New Arrival</span>
+          <span className="xl:bg-white xl:whitespace-nowrap
+            relative z-10 pr-10">
+            The New Arrival
+          </span>
           <br />
-          <span className="text-coral-red">Nike </span>
-          Shoes
+          <span className="text-coral-red inline-block mt-3">
+            Nike 
+          </span> Shoes
           </h1>
-          <p>
+          <p className="font-montserrat text-slate-gray text-lg
+            leading-8 mt-6 mb-14 sm:max-w-sm">
             Discover stylish Nike arrivals, quality comfort, 
             and innovation to help you look and feel your best.
           </p>
@@ -33,16 +47,45 @@ const Hero = () => {
             iconUrl={arrowRight}
           />
 
-          <div className="flex justify-start items-start flex-wrap
-            w-full mt-20 gap-16">
+          <div className="flex justify-start items-start 
+            flex-wrap w-full mt-20 gap-16">
             {statistics.map((statistic) => 
               <div key={statistic.label}>
-                <p>{statistic.value}</p>
-                <p>{statistic.label}</p>
+                <p className="text-4xl font-palanquin font-bold">
+                  {statistic.value}
+                </p>
+                <p className="leading-7 font-montserrat text-slate-gray">
+                  {statistic.label}
+                </p>
               </div>
             )}
           </div>
+      </div>
 
+      <div className="relative flex-1 flex justify-center 
+        items-center xl:min-h-screen max-xl:py-40 bg-primary
+        bg-hero bg-cover bg-center">
+        <img 
+          src={bigShoeImg}
+          alt="shoe collection"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%]
+          sm:left-[10%] max-sm:px-6"
+        >
+          {shoes.map((shoe) =>(
+            <div key={shoe}>
+              <ShoeCard 
+                imgURL={shoe}
+                changeBigShoeImage=
+                {(shoe) => {setBigShoeImg(shoe)}}
+                bigShoeImg={bigShoeImg}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )
