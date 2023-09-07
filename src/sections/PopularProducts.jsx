4ea561a145785e05575fce4a6271e9dc81fc6f11
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-09-07 18:32:07 -0300

    implementing the products section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,6 +1,18 @@
+import { products } from "../constants"
+
 const popularProducts = () => {
   return (
-    <div>popularProducts</div>
+    <div>
+      <section 
+        id="products"
+        className="max-container max-sm:mt-12"
+      >
+        <div className="flex flex-col justify-start gap-5">
+          <h2 className="text-center text-3xl font-bold mb-8">Popular Products</h2>
+        </div>
+
+      </section>
+    </div>
   )
 }
 
