cd539f426af34964b6cda02e49b25b09ee9cf6da
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-11-05 21:13:36 -0300

    imp:footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,74 @@
+import { copyrightSign } from '../assets/icons'
+import { footerLogo } from '../assets/images/'
+import { socialMedia, footerLinks } from '../constants'
+
 const Footer = () => {
   return (
     <footer className="max-container">
-      
+      <div className="flex justify-between items-start gap-20
+        flex-wrap max-lg:flex-col">
+          <div className="flex flex-col items-start">
+            <a href="/">
+              <img 
+                src={footerLogo}
+                width={150}
+                height={46}
+                alt="Logo" />
+            </a>
+            <p className="mt-6 text-base font-montserrat leading-7
+              text-white-400 sm:max-w-sm">
+                Get shoes ready for the new term at your nearest Nike store. Find Your perfect Size In Store. Get Rewards
+            </p>
+            <div className="flex items-center gap-5 mt-8">
+              {socialMedia.map((icon) =>(
+                <div className="flex justify-center items-center w-12
+                  h-12 bg-white rounded-full">
+                  <img 
+                    src={icon.src} 
+                    alt={icon.alt}
+                    width={24}
+                    height={24} 
+                    />
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap" >
+            {
+              footerLinks.map((section) => (
+                <div key={section}>
+                  <h4 className="text-white-400 font-montserrat text-2xl
+                    leading-normal font-medium mb-6">
+                    {section.title}
+                  </h4>
+                  <ul className="">
+                    {section.links.map((link) => (
+                      <li key={link.name} className="mt-3 text-white-400 font-montserrat text-base leading-normal
+                        hover:text-slate-gray cursor-pointer">
+                        <a href={link.link}>{link.name}</a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+          </div>
+      </div>
+
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 items-center justify-start gap-2 font-montserrat cursor-pointer hover:text-slate-gray">
+          <img 
+            src={copyrightSign}
+            width={20}
+            height={20}
+            alt="copi right sign"
+            className="rounded-full m-0"
+          />
+          <p>Copyright. All rights reserved.</p>
+        </div>
+        <p className="font-montserrat"><a href="/" className="hover:text-slate-gray">Terms & Conditions</a></p>
+
+      </div>
     </footer>
   )
 }
